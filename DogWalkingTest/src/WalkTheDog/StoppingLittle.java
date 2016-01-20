386d47cc57e5e66f7baee6cc74d78354413a694d
Ramzuous <Ramzuous@42.0.121.144>
2016-01-20 12:46:40 +0100

    first

---
+++ DogWalkingTest/src/WalkTheDog/StoppingLittle.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WalkTheDog;
+
+/**
+ *
+ * @author Ramzuous
+ */
+public class StoppingLittle {
+    
+    public static void stop(){
+        
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e){}
+        
+    }
+    
+}
